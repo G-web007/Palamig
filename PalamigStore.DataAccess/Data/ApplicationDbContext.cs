420916822b37d9ea060c84cde480d725e4aadd08
vincent <ygbuhayv@gmail.com>
2024-09-24 12:58:24 +0800

    Adding Category Seeder and migration

--- PalamigStore.DataAccess/Data/ApplicationDbContext.cs
+++ PalamigStore.DataAccess/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace PalamigStore.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Ice Candy", Quantity = 45 },
+                new Category { Id = 2, Name = "Popsicle", Quantity = 20 },
+                new Category { Id = 3, Name = "Granita", Quantity = 32 }
+            );
+        }
     }
 }
