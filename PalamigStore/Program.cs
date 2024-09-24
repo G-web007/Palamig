d3733e79c18a124a408742b9801d3950f5289af4
vincent <ygbuhayv@gmail.com>
2024-09-24 10:22:33 +0800

    Starup code in the Program.cs

--- PalamigStore/Program.cs
+++ PalamigStore/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment()) 
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
 
 app.Run();
