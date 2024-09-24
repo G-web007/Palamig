4b162c1d0d6768c2e7258f3e584655bfc0b513e7
vincent <ygbuhayv@gmail.com>
2024-09-24 10:56:54 +0800

    Create Category class and properties

---
+++ PalamigStore.Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PalamigStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+    }
+}
