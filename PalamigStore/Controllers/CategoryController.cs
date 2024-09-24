c01245513b3b5f1e16e97a60d4ab179c9acea7c1
vincent <ygbuhayv@gmail.com>
2024-09-24 13:15:17 +0800

    CategoryController and Index

---
+++ PalamigStore/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PalamigStore.DataAccess.Data;
+using PalamigStore.Models;
+
+namespace PalamigStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryFromDb = _context.Categories.ToList();
+            return View(categoryFromDb);
+        }
+    }
+}
