cb6edae7e889aee5d9acd40b6866adbeaebda78c
vincent <ygbuhayv@gmail.com>
2024-09-25 11:17:20 +0800

    Adding Functionalities in Product Controller, Forms, and Link for Edit and Delete

--- PalamigStore/Areas/Admin/Controllers/ProductController.cs
+++ PalamigStore/Areas/Admin/Controllers/ProductController.cs
@@ -23,5 +23,80 @@ namespace PalamigStore.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product? ProductFromDb = _unitOfWork.Product.Get(x => x.Id == id);
+
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _unitOfWork.Product.Get(x => x.Id == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(x => x.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
