a82773647338605026e9bb61517f5e1d27e14ad8
vincent <ygbuhayv@gmail.com>
2024-09-25 11:02:06 +0800

    Product implementation from Controller, Views, and UnitOfWord

---
+++ PalamigStore.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using PalamigStore.Models;
+
+namespace PalamigStore.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
---
+++ PalamigStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PalamigStore.DataAccess.Repository.IRepository;
+using PalamigStore.Models;
+
+namespace PalamigStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> productsFromDb = _unitOfWork.Product.GetAll().OrderByDescending(p => p.Id).ToList();
+            return View(productsFromDb);
+        }
+    }
+}
