7f867940b2eeac09807c9c5be6dc65f395b62b77
vincent <ygbuhayv@gmail.com>
2024-09-26 10:58:04 +0800

    Implementing TinyMCE

---
+++ PalamigStore/wwwroot/js/main.js
@@ -0,0 +1,6 @@
+tinymce.init({
+    selector: 'textarea',
+    plugins: 'anchor autolink charmap codesample emoticons image link lists table visualblocks wordcount',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | checklist numlist bullist indent outdent',
+});
+
